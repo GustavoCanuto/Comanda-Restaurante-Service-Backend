b4026c1ebad71ad5f245d07e4b1c68e52256b5de
Gustavo Canuto <gcanuto.dev@gmail.com>
2024-11-19 21:22:53 -0300

    bugfix: retornando token junto com 403

--- comandaRestaurante/src/main/java/com/projetoweb4/comandaRestaurante/infra/security/SecurityFilter.java
+++ comandaRestaurante/src/main/java/com/projetoweb4/comandaRestaurante/infra/security/SecurityFilter.java
@@ -8,7 +8,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import com.projetoweb4.comandaRestaurante.entity.domain.StatusGeral;
 import com.projetoweb4.comandaRestaurante.enumeration.StatusGeralEnum;
 import com.projetoweb4.comandaRestaurante.repository.LoginRepository;
 import com.projetoweb4.comandaRestaurante.service.recurso.TokenService;
@@ -31,6 +30,7 @@ public class SecurityFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var tokenJWT = recuperarToken(request);
 
+        try {
         if (tokenJWT != null) {
             var subject = tokenService.getSubject(tokenJWT);
             var usuario = repository.findByEmailAndStatusGeral_Id(subject, StatusGeralEnum.ATIVO.getId());
@@ -44,6 +44,14 @@ public class SecurityFilter extends OncePerRequestFilter {
         }
 
         filterChain.doFilter(request, response);
+        
+        } catch (Exception ex) {
+            // Configura a resposta com status 403 e inclui o token
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.setContentType("application/json");
+            response.getWriter().write("{\"error\": \"Acesso negado\", \"tokenRecebido\": \"" + (tokenJWT != null ? tokenJWT : "Nenhum token enviado") + "\"}");
+            response.getWriter().flush();
+        }
     }
 
     private String recuperarToken(HttpServletRequest request) {
